dc4380b135786c3e7ddc1f0254b3c105ecb51705
saff84 <saff84@mail.ru>
2022-05-24 06:44:26 +0300

    modify ypravlenie and requirements

--- ypravlenie.py
+++ ypravlenie.py
@@ -6,66 +6,52 @@ import requests
 import os
 import time
 
-# name_directory = input('Введите название новой папки для загрузки фотографий: ')
-# if not os.path.isdir(name_directory):
-#      os.mkdir(name_directory)
-#      print(f'Папка {name_directory} создана')
-# else:
-#     print('Такая папка уже существует')
-token = "a67f00c673c3d4b12800dd0ba29579ec56d804f3c5f3bbcef5328d4b3981fa5987b951cf2c8d8b24b9abd"
-vk_client = VkUser(token, '5.131')
-vk_id = int(input('Введите id ')) #552934290
-# time.sleep(3)
-#Получаем данные с vk и пишем в файл
-photos = vk_client.photos_get(vk_id)
-with open('photos.json', "w", encoding="utf-8") as f:
-    json.dump(photos, f)
-
-#Достаем из файла из вк нужные данные и формируем названия фотографий в соответствии с требованиями
-with open('photos.json') as json_file:
-    data = json.load(json_file)
-# pprint(data)
-spisok_photo = []
-for photo in data:
-    if str(photo['likes']['count']) + ".jpg" in list(map(lambda x: x['file_name'], spisok_photo)):
-        spisok_photo.append({"file_name": str(photo['likes']['count']) + "_" + str(photo['date']) + ".jpg", "size":
-            photo['sizes'][photo['sizes'].index(max(photo['sizes'], key=lambda s: s['height'] * s['width']))]['type'], 'url': photo['sizes'][photo['sizes'].index(max(photo['sizes'], key=lambda s: s['height'] * s['width']))]['url']})
-    else:
-        spisok_photo.append({"file_name": str(photo['likes']['count']) + ".jpg", "size":
-            photo['sizes'][photo['sizes'].index(max(photo['sizes'], key=lambda s: s['height'] * s['width']))]['type'], 'url': photo['sizes'][photo['sizes'].index(max(photo['sizes'], key=lambda s: s['height'] * s['width']))]['url']})
-# pprint(spisok_photo)
-
-#Загружаем файлы с соответствющими названиями в отдельную папку на компьютер
-# for photo in spisok_photo:
-#     response = requests.get(photo['url'])
-#     filename = f'{photo["file_name"]}'
-#     if response.status_code == 200:
-#         with open(f'{name_directory}/{filename}',"wb") as f:
-#             f.write(response.content)
 
 if __name__ == '__main__':
+
+    token_vk = "a67f00c673c3d4b12800dd0ba29579ec56d804f3c5f3bbcef5328d4b3981fa5987b951cf2c8d8b24b9abd"
+    vk_client = VkUser(token_vk, '5.131')
+    vk_id = input('Введите id ')  # 552934290
+
+    # Получаем данные с vk и пишем в файл
+    photos = vk_client.photos_get(vk_id)
+
+    # Достаем из файла из вк нужные данные и формируем названия фотографий в
+    # соответствии с требованиями
+    with open('photos.json', "w", encoding="utf-8") as f:
+        json.dump(photos, f)
+
+    with open('photos.json') as json_file:
+        data = json.load(json_file)
+    # pprint(data)
+
+    # формируем список с нужными данными из vk
+    spisok_photo = []
+    for photo in data:
+        if str(photo['likes']['count']) + \
+                ".jpg" in list(map(lambda x: x['file_name'], spisok_photo)):
+            spisok_photo.append({"file_name": str(photo['likes']['count']) + "_" + str(photo['date']) + ".jpg", "size":
+                                 photo['sizes'][photo['sizes'].index(max(photo['sizes'], key=lambda s: s['height'] * s['width']))][
+                'type'], 'url': photo['sizes'][
+                photo['sizes'].index(max(photo['sizes'], key=lambda s: s['height'] * s['width']))]['url']})
+        else:
+            spisok_photo.append({"file_name": str(photo['likes']['count']) + ".jpg", "size":
+                                 photo['sizes'][photo['sizes'].index(max(photo['sizes'], key=lambda s: s['height'] * s['width']))][
+                'type'], 'url': photo['sizes'][
+                photo['sizes'].index(max(photo['sizes'], key=lambda s: s['height'] * s['width']))]['url']})
     # token = ''
-    token = input('Введите токен для авторизации на Яндекс Диске: ')
-    uploader = YaUploader(token)
+
+    token_ya = input('Введите токен для авторизации на Яндекс Диске: ')
+    uploader = YaUploader(token_ya)
     upload_path = input('Введите название папки для загрузки: ')
     uploader.new_folder(upload_path)
-
-    # Загружаем файлы с соответствющими названиями из отдельной папки на компьютере в Яндекс Диск
-    # for photo in spisok_photo:
-    #     file = f'{name_directory}/{photo["file_name"]}'
-    #     path_to_file = f'{os.path.split(file)[1]}'
-    #     #p_bar =
-    #     print(f'Загружаю файл {path_to_file} в папку {upload_path}')
-    #     result = uploader.upload_file(f'{upload_path}/{os.path.split(file)[1]}',file)
-    # print('Загрузка завершена')
 
     # Загружаем файлы с соответствющими названиями по url в ВК  в Яндекс Диск
     for photo in spisok_photo:
         file = f'{photo["file_name"]}'
         url = f'{photo["url"]}'
-        #p_bar =
+        # p_bar =
         print(f'Загружаю файл {file} в папку {upload_path}')
         result = uploader.upload_file(f'{upload_path}/{file}', url)
-
 
     print('Загрузка завершена')
